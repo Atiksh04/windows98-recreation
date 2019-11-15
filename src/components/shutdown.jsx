26f1d96d4e2a9b9ab4a6aa323afb796f32084149
Atiksh Srivastava <ayushs125@gmail.com>
2019-11-16 01:52:21 +0530

    changed shutdown

--- src/components/shutdown.jsx
+++ src/components/shutdown.jsx
@@ -4,6 +4,8 @@ export default class Startup extends React.Component{
 	render(){
 		return(
 			<div className="startup_background">
+
+			<p className="lead">Windows is Ready to Shutdown.</p>
 			<audio autoPlay>
 				<source src={sound} type="audio/mpeg"/>
 				Your browser does not support the audio tag.
